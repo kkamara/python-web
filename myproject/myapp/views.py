e59e64febfa1cc94839e61ce3589c5e27161fa8a
kkamara <kamaracomputers@gmail.com>
2023-07-15 22:37:31 +0100

    Add user registration

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
@@ -7,6 +9,26 @@ def index(request):
   features = Feature.objects.all()
   context = { 'features': features}
   return render(request, 'index.html', context)
+
+def register(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    if password == password2:
+      if User.objects.filter(email=email).exists():
+        messages.info(request, 'Email already exists.')
+        return redirect('register')
+      elif User.objects.filter(username=username).exists():
+        messages.info(request, 'Username already exists.')
+        return redirect('register')
+      else:
+        user = User.objects.create_user(username=username, email=email,password=password)
+        user.save()
+        return redirect('login')
+  return render(request, 'register.html')
 
 def counter(request):
   text = request.POST['text']
