b8c7053acdd339e12bd93d9f95f69eed07be86a4
kkamara <kamaracomputers@gmail.com>
2023-07-15 16:23:24 +0100

    Add template variables

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    name = 'Jane Doe'
+    return render(
+        request, 'index.html', {'name': name,})
