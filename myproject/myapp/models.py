29242048ce72580a6fdf0d9db02b55576b1e3397
kkamara <kamaracomputers@gmail.com>
2023-07-15 22:08:19 +0100

    Add admin features, dynamic data and migrations

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Model View Template
 # Create your models here.
-class Feature:
-  id: int
-  name: str
-  details: str 
-  isTrue: bool
+class Feature(models.Model):
+  name = models.CharField(max_length=100)
+  details = models.CharField(max_length=500)
